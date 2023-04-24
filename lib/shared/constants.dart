d90e376c0f2e24086b1c384bfb78465726a9fb15
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-04-24 21:57:06 +0530

    Initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+  static String appId="1:486197886687:web:46ea0bea81e0de8a27b36a";
+  static String apiKey="AIzaSyBYgL15W4jQH165lE_MuZjKfAx3jpzIWNM";
+  static String messagingSenderId="486197886687";
+  static String projectId="chatapp-b83aa";
+  final primaryColor = const Color(0xFFee7b64);
+}
